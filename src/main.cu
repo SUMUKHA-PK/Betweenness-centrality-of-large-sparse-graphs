16bf7cf2283627dfb56a23c34185fd59f1cb85bf
26prajval98 <26prajval98@gmail.com>
2018-11-24 09:47:21 +0530

    T

--- src/main.cu
+++ src/main.cu
@@ -1,139 +1,142 @@
-#include <iostream>
-#include <vector>
+//Sumukha Pk 16CO145
+//Prajval M  16CO234
 
-#include <thrust/device_vector.h>
-#include <thrust/host_vector.h>
-#include <thrust/functional.h>
-#include <thrust/transform.h>
-#include <thrust/extrema.h>
-
-#include "cuda_runtime.h"
-#include "device_launch_parameters.h"
-
-#define THREADS 64
+#include<cuda.h>
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<string>
+#include"graphs.cuh"
 
 using namespace std;
-using namespace thrust;
+using namespace graph;
 
-typedef struct vertex {
-    int parents[15];
-    int no_parents;
-    int distance;
-    bool done;
-} Vertex;
-
-typedef struct qu {
-    int id;
-    int distance;
-} Queue;
-
-typedef struct edge{
-    int tos[10];
-    int no_to;
-} Edge;
-
-struct compare_key_value{
-    __host__ __device__
-    bool operator()(Queue l, Queue r) {
-        return l.distance < r.distance;
-    }
-};
-
-struct compare_key_value qSelector;
-
-int ExtractMin(device_vector<Queue> &queue){
-
-    device_vector<Queue>::iterator t = min_element(queue.begin(), queue.end(), qSelector);
-
-    Queue x = *t;
-    
-    queue.erase(t);
-
-    return x.id;
-}
-
-__global__
-void relax(Vertex * nodes, int * edges, int id, int dist, int no_edges){
-    int idx = threadIdx.x + blockDim.x * blockIdx.x;
-
-    if(idx < no_edges){
-        if(nodes[idx].done == false){
-            if(nodes[idx].distance > dist + 1){
-                nodes[idx].distance = dist;
-                nodes[idx].parents[nodes[idx].no_parents] = id;
-                nodes[idx].no_parents = 1;
+int main(int argc,char ** argv)
+{
+    FILE *fp;
+    fp = fopen(argv[argc-1], "r");
+    char c = fgetc(fp); 
+    int flag = -1;
+    long long int i,j,a=0,nnz=2,b=0,count=0;
+    while (c != EOF) 
+    { 
+        if(c=='\n')
+        {
+            if(flag==-1)
+            {
+                a=count;
+                count=0;
+                flag++;
             }
-            else if(nodes[idx].distance == dist + 1){
-                nodes[idx].parents[nodes[idx].no_parents - 1] = id;
-                nodes[idx].no_parents += 1;
+            else if(flag==0)
+            {
+                b=count;
+                count=0;
+                flag++;
             }
         }
-    }
+        else if(c==' ') ++count;
+        c = fgetc(fp); 
+    } 
+    fclose(fp);
+    fp = fopen(argv[argc-1], "r");
+    c = fgetc(fp);
+    long long int *n1 = (long long int *)malloc(a*sizeof(long long int));
+    long long int *n2 = (long long int *)malloc(b*sizeof(long long int));
+    long long int *n3 = (long long int *)malloc(a*sizeof(long long int));
+    long long int *n4 = (long long int *)malloc(nnz*sizeof(long long int));
+    count=0;
+    string x = "";
+    while (c != EOF) 
+    { 
+        if(c!=' ')
+        {
+            x+=c;
+        } 
+        else if(c==' '||c=='\n')
+        {
+            if(count<a)
+            {
+                n1[count]=stoi(x);
+                x="";
+                count++;
+            }   
+            else if(count>=a&&count<(a+b))
+            {
+                n2[count-(a)]=stoi(x);
+                x="";
+                count++;
+            }
+            else if(count>=(a+b)&&count<(2*a+b))
+            {
+                n3[count-(a+b)]=stoi(x);
+                x="";
+                count++;
+            }
+            else
+            {
+                n4[count-(2*a+b)]=stoi(x);
+                x="";
+                count++;
+            }
+        }
+       c = fgetc(fp); 
+    } 
+    n4[1]=stoi(x);
+    fclose(fp);
 
-}
+//-------------------------------------------File input done------------------------------
 
-int dijkstra(int source, int target, int no_nodes, vector<Edge> es){
+    long long int dim2=n4[0],dim1=n4[1];
+    Edge * edges = new Edge[dim1];   //Number of edges is size of n1
+    Vertex * vertices = new Vertex[dim2];
 
-    host_vector<Vertex> h_nodes(no_nodes);
-    host_vector<Queue> h_queue(no_nodes);
-    host_vector<Edge> h_edges(es);
-    host_vector<Queue> temp(no_nodes);
-
-    for(int i = 0; i < no_nodes; i++){
-        h_queue[i].id = i;
-        h_queue[i].distance = INT_MAX;
-        h_nodes[i].distance = INT_MAX;
-        h_nodes[i].no_parents = 0;
-        h_nodes[i].done = true;
-    }
-
-    h_queue[source].distance = 0;
-    h_nodes[source].distance = 0;
-
-    device_vector<Vertex> d_nodes(h_nodes);
-    device_vector<Queue> d_queue(h_queue);
-    device_vector<Edge> d_edges(h_edges);
-
-    Vertex * d_nodes_ptr = raw_pointer_cast(d_nodes.data());
-    Edge * d_edges_ptr = raw_pointer_cast(d_edges.data());
+    for(i=0;i<dim1;i++)
+        edges[i]->item=NULL;
     
-    for(int i = 0; i < no_nodes - 1; i++){
-        int id = ExtractMin(d_queue);
-        int no_edges = h_edges[id].no_to;
-        int * edges;
-
-        cudaMalloc((void **)edges, no_edges*sizeof(int));
-        cudaMemCpy(edges, )
+    long long cc =0,cd=0,ignore=0;
+    for(i=1;i<=dim1;i++)
+    {
+        vertices[i-1].id=i-1;
+        long long x = n2[i]-n2[i-1];
+        for(j=cc;j<cc+x;j++)
+        {
+            if((i-1)!=n3[j])
+            {
+                edges[i-1].from=(i-1);
+                edges[cd].to=n3[j];
+                cd++;
+                Item * temp = edges[i-1].item;
+                while(temp->item!=NULL)
+                    temp=temp->item;
+                Item *t = new Item;
+                temp->item=t;
+                t->item=NULL;
+            }
+            else
+            {   
+                ignore++;
+            }
+        }
+        cc+=x;
     }
+    a-=ignore;
+    // for(i=0;i<a;i++)
+    // {
+    //     cout<<edges[i].from<<" "<<edges[i].to<<endl;
+    // }
+    
+    for(i=a;i<2*a;i++)
+    {
+        edges[i].from=edges[i-a].to;
+        edges[i].to=edges[i-a].from;
+    }
+    free(n1);
+    free(n2);
+    free(n3);
+    free(n4);
+//---------------------------------------------Graph is generated--------------------------
 
+    
     return 0;
-}
-
-int main(){
-
-    int B[5][5] = {
-        {0, 1, 0, 1, 0},
-        {1, 0, 1, 0, 0},
-        {0, 1, 0, 1, 1},
-        {1, 0, 1, 0, 0},
-        {0, 0, 1, 0, 0},
-    };
-
-    vector<Edge> es; 
-
-    for(int i=0; i < 5; i++){
-        Edge temp;
-        temp.no_to = 0;
-
-        es.push_back(temp);
-
-        for(int j=0; j < 5; j++){
-            if(B[i][j] == 1){
-                es[i].tos[es[i].no_to] = j;
-                es[i].no_to += 1;
-            }
-        }
-    }
-
-    dijkstra(0, 4, 5, es);
 }

ce31937babaa8be194a3a036eb59f3f17ae907dd
26prajval98 <26prajval98@gmail.com>
2018-11-24 13:59:42 +0530

    Added device

--- src/main.cu
+++ src/main.cu
@@ -12,6 +12,29 @@
 using namespace std;
 using namespace graphs;
 
+void printDevProp(cudaDeviceProp devProp){
+    
+    // printf("Compute Capability:            %d.%d\n",  devProp.major, devProp.minor);
+    printf("Device Name:                   %s\n",  devProp.name);
+    // printf("Total global memory:           %zu\n",  devProp.totalGlobalMem);
+    // printf("Total shared memory per block: %zu\n",  devProp.sharedMemPerBlock);
+    // printf("Total registers per block:     %d\n",  devProp.regsPerBlock);
+    // printf("Warp size:                     %d threads\n",  devProp.warpSize);
+    // printf("Maximum memory pitch:          %zu\n",  devProp.memPitch);
+    // printf("Maximum threads per block:     %d\n",  devProp.maxThreadsPerBlock);
+    // for (int i = 0; i < 3; ++i)
+    // 	printf("Maximum dimension %d of block:  %d\n", i, devProp.maxThreadsDim[i]);
+    // for (int i = 0; i < 3; ++i)
+    // 	printf("Maximum dimension %d of grid:   %d\n", i, devProp.maxGridSize[i]);
+    printf("Clock rate:                    %d\n",  devProp.clockRate);
+    // printf("Total constant memory:         %zu\n",  devProp.totalConstMem);
+    // printf("Texture alignment:             %zu\n",  devProp.textureAlignment);
+    // printf("Concurrent copy and execution: %s\n",  (devProp.deviceOverlap ? "Yes" : "No"));
+    // printf("Number of multiprocessors:     %d\n",  devProp.multiProcessorCount);
+    // printf("Kernel execution timeout:      %s\n",  (devProp.kernelExecTimeoutEnabled ? "Yes" : "No"));
+    return;
+}
+
 int main(int argc,char ** argv)
 {
     FILE *fp;
@@ -112,7 +135,11 @@ int main(int argc,char ** argv)
     }
     a-=ignore;
 
-    cout << "Strarted Computing ...." << endl;
+    cudaDeviceProp devProp;
+
+    printDevProp(devProp);
+
+    cout << "Started Computing ...." << endl;
 
     clock_t begin = clock();
 
